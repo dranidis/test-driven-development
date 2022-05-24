0906f70f44c0b6bd7fb9573f9a4d55aa0de9ec88
Dimitris Dranidis <dranidis@gmail.com>
2022-05-24 16:30:37 +0300

    Works for 1

---
+++ src/main/java/com/factors/Factor.java
@@ -0,0 +1,12 @@
+package com.factors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factor {
+
+    public static List<Integer> getFactors(int number) {
+        return new ArrayList<>();
+    }
+
+}
--- src/test/java/com/factors/FactorTest.java
+++ src/test/java/com/factors/FactorTest.java
@@ -1,12 +1,14 @@
 package com.factors;
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class FactorTest {
 
 	@Test
-	public void dummy() {
-		assertEquals(1, 2);
+	public void getFactors_of_1_is_empty_list() {
+		assertEquals(Arrays.asList(), Factor.getFactors(1));
 	}
 }
